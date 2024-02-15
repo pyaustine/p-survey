fbf0dedf449ac10b1e36c4f5ef21a4eb53b23713
sphinx-austin <austinjuniour4@gmail.com>
2024-02-15 10:33:13 +0300

    update

---
+++ api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Name(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Questionnaire(models.Model):
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+    class Meta:
+        ordering = ['-updated']        
