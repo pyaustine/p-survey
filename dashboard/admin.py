8dc8a7c10f0184c0b4cf38bbd8e9938701f3d983
sphinx-austin <austinjuniour4@gmail.com>
2024-02-15 10:34:20 +0300

    update

---
+++ dashboard/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+
+
+admin.site.register(Designations)
+admin.site.register(Survey)
+admin.site.register(QuestionnaireApi)
+# admin.site.register(TestQuestionnaire)
+admin.site.register(Report)
